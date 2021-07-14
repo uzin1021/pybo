078b23166ee18a9b499e9c79999ff0c5bdc49218
uzin1021 <you3554@naver.com>
2021-07-14 21:47:04 +0900

    "django start"

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from pybo import views
 
 urlpatterns = [
-    path('', views.index)
+    path('' , views.index)
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question, Answer
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,13 @@ def index(request):
     context = {'question_list' : question_list}
     return render(request, 'pybo/question_list.html',context)
 
+def detail(request, question_id):
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context ={'question' : question}
+    return render(request, 'pybo/question_detail.html', context)
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
